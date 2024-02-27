f88266d2edd9147b4787d1daa83875c45bd3526e
Nokimalos <kaan.bouldoires@epitech.eu>
2024-02-27 15:37:36 +0100

    fix: location of files + optimized code

--- src/password_generator.lua
+++ src/password_generator.lua
@@ -1,5 +1,10 @@
 local sha2 = require("sha2")
 
+local VERY_WEAK = 6
+local WEAK = 8
+local AVERAGE = 10
+local STRONG = 12
+
 function generatePassword(length)
     local charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
     local password = ""
@@ -15,18 +20,21 @@ end
 
 function evaluatePasswordStrength(password)
     local length = #password
-    if length < 6 then
-        return "Very weak"
-    elseif length < 8 then
-        return "Weak"
-    elseif length < 10 then
-        return "Average"
-    elseif length < 12 then
-        return "Strong"
-    else
-        return "Very Strong"
+    local strengthLevels = {
+        {VERY_WEAK, "Very weak"},
+        {WEAK, "Weak"},
+        {AVERAGE, "Average"},
+        {STRONG, "Strong"}
+    }
+
+    for _, level in ipairs(strengthLevels) do
+        if length < level[1] then
+            return level[2]
+        end
     end
+    return "Very Strong"
 end
+
 
 function handleArgument()
     local length = io.read("*n")    
@@ -35,7 +43,7 @@ function handleArgument()
     local encrypt = sha2.sha256(password)
 
     if length == 0 then
-        print("This not possible, you need at least one caractere in your password")
+        print("This not possible, you need at least one caractere in your password !")
     else
         print("Generated password : " .. password)
         print("Password strength : " .. strength)
